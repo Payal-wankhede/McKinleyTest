5c4aaa21cbad3ee9b8463026e3721d9a930da6b4
Rajat Bhatt <r.bhatt@easternenterprise.com>
2020-03-12 20:14:46 +0530

    Initial Commit

---
+++ McKinLey&RiceTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McKinLey&RiceTest
+//
+//  Created by Rajat Bhatt on 12/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
